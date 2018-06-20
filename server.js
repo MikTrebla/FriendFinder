3a72fcfbe2e3ad8e9cb465161c0bfbfa9d5b1f4c
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-19 17:17:27 -0700

    created basic html files

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
+
+
+// app.use(express.static(path.join(__dirname,'/public')));
+// app.use(express.static(path.join(__dirname, '/routing')));
+app.use('/', express.static('public'));
+app.use('/', express.static('routing'));
+
+
+
+app.listen(PORT, function () {
+    console.log('APP listening on PORT ' + PORT);
+});
