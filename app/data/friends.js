ca1c2dde8a07ead053ab2726c3361181ef1bd658
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-21 16:21:35 -0700

    finished?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 let friendList = [{
         name: "Ahmed",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo: "https://media.giphy.com/media/a9A3HLylBz2yA/giphy.gif",
         score: [
             '5', '1', '4', '4', '5', '1', '2', '5', '4', '1'
         ]
