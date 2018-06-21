4d816a320c622f0c198e41c54d626b488e549a1f
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-20 19:36:36 -0700

    completed math logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,14 +2,14 @@ let friendList = [{
         name: "Ahmed",
         photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         score: [
-            5, 1, 4, 4, 5, 1, 2, 5, 4, 1
+            '5', '1', '4', '4', '5', '1', '2', '5', '4', '1'
         ]
     },
     {
         name: 'Albert',
         photo: 'https://www.google.com',
         score: [
-            1, 2, 3, 4, 5, 1, 2, 3, 4, 5
+            '1', '2', '3', '4', '5', '1', '2', '3', '4', '5'
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -25,36 +25,33 @@ router.post('/friends', function (req, res) {
     res.json(newFriend);
 
     let userScore = newFriend.score;
-    // console.log(data[0].score);
-    // for (var i = 0; i < userScore.length; i++) {
-    //     console.log(parseInt(userScore[i]));
-    // }
-    // console.log(parseInt(userScore));
+
+    console.log(parseInt(userScore));
     for (var i = 0; i < data.length; i++) {
-        let compareDifference = 40;
+        let matchThreshold = 40;
         let difference = 0;
-        
+        console.log('data score: ' + data[i].score);
         for (var j = 0; j < userScore.length; j++) {
-            difference += Math.abs(parseInt(userScore[j]) - parseInt(data[i].score));
-            console.log(difference);
-            if (compareDifference > difference) {
-                
+            console.log(difference += Math.abs(parseInt(userScore[j]) - parseInt(data[i].score[j])));
+            console.log('userScore: ' + userScore)
+            if (matchThreshold > difference) {
+
             }
-        }
-    }
+        };
+    };
 });
 
 
 
 
 
-router.get('/friends/:friends', function (req, res) {
-    var friendName = req.params.friends;
+// router.get('/friends/:friends', function (req, res) {
+//     var friendName = req.params.friends;
 
-    for (var i = 0; i < data.length; i++) {
-        if (friendName === data[i].routeName) {
-            return res.json(data[i]);
-        }
-    }
-    return res.json(false);
-});
+//     for (var i = 0; i < data.length; i++) {
+//         if (friendName === data[i].routeName) {
+//             return res.json(data[i]);
+//         }
+//     }
+//     return res.json(false);
+// });
