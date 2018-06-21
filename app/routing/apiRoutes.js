fb6006c425c2a38cef53f2ca1c53162e27c7e49b
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-06-20 18:48:34 -0700

    added score difference logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,24 +18,43 @@ router.get('/friends', function (req, res) {
 module.exports = router;
 
 router.post('/friends', function (req, res) {
-    var newFriend = req.body;
+    let newFriend = req.body;
     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
     console.log(newFriend);
     data.push(newFriend);
     res.json(newFriend);
+
+    let userScore = newFriend.score;
+    // console.log(data[0].score);
+    // for (var i = 0; i < userScore.length; i++) {
+    //     console.log(parseInt(userScore[i]));
+    // }
+    // console.log(parseInt(userScore));
+    for (var i = 0; i < data.length; i++) {
+        let compareDifference = 40;
+        let difference = 0;
+        
+        for (var j = 0; j < userScore.length; j++) {
+            difference += Math.abs(parseInt(userScore[j]) - parseInt(data[i].score));
+            console.log(difference);
+            if (compareDifference > difference) {
+                
+            }
+        }
+    }
 });
 
 
 
 
 
-// app.get('/friends/:friends', function (req, res) {
-//     var friendName = req.params.friends;
-    
-//     for (var i = 0; i < friendName.length; i++) {
-//         if (friendName === friendName[i].routeName) {
-//             return res.json(friends[i]);
-//         }
-//     }
-//     return res.json(false);
-// });
+router.get('/friends/:friends', function (req, res) {
+    var friendName = req.params.friends;
+
+    for (var i = 0; i < data.length; i++) {
+        if (friendName === data[i].routeName) {
+            return res.json(data[i]);
+        }
+    }
+    return res.json(false);
+});
